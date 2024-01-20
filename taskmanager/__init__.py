b5b5238b91e9282a277d785a1b7b44bd00bf3ff6
michal-mrozek <>
2024-01-20 00:08:16 +0000

    last one

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -7,13 +7,25 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+if os.eapp = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
-     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
- else:
-     uri = os.environ.get("DATABASE_URL")
-     if uri.startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-     app.config["SQLALCHEMY_DATABASE_URI"] = uri
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqanviron.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
